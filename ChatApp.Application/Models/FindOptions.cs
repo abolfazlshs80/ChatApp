3a56ddeff0603bf6dd09e2c41e85a1ba072494b1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-11 10:07:24 +0330

    Add FindOptions class for query configuration options

---
+++ ChatApp.Application/Models/FindOptions.cs
@@ -0,0 +1,8 @@
+namespace ChatApp.Application.Models
+{
+    public class FindOptions
+    {
+        public bool IsIgnoreAutoIncludes { get; set; }
+        public bool IsAsNoTracking { get; set; }
+    }
+}
