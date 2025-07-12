00d548093f8e6017966178ed68606d42aa66e202
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-12 11:13:41 +0330

    Add ChatMessageService implementation for chat message functionalities

---
+++ ChatApp.Application/Features/Services/MessageService.cs
@@ -0,0 +1,16 @@
+using ChatApp.Application.Features.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Application.Features.Services
+{
+    public class ChatMessageService : IChatMessageService
+    {
+    }
+
+
+}
