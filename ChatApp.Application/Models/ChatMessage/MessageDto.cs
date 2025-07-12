58c7e9738af8de4d2da3335e78cc327133aca3ed
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-12 11:17:54 +0330

    Add messageDto

---
+++ ChatApp.Application/Models/ChatMessage/MessageDto.cs
@@ -0,0 +1,8 @@
+namespace ChatApp.Application.Models.ChatMessage;
+
+public class MessageDto
+{
+    public string Sender { get; set; }
+    public string Message { get; set; }
+    public DateTime Time { get; set; }
+}
